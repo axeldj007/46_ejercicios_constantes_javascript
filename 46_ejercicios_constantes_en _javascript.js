18540d291fd4e96aa978f2cebf771b8198ab45ca
axeldj007 <axelku9@gmail.com>
2025-08-05 10:15:33 -0600

    Add files via upload

---
+++ 46_ejercicios_constantes_en _javascript.js
@@ -0,0 +1,462 @@
+let edad = 25;
+let temperatura = 65.4;
+let haceCalor = true;
+let ciudad = 'Aguascalientes';
+let registroHumedad = null;
+let hayLluvia = undefined;
+const PAIS = 'Mexico';
+console.log('En ' + ciudad +' que esta en '+ PAIS + " la temperatura es de "+ temperatura + ' grados');
+
+let cantidadTareas = 35;
+let calPromedio = 85.4;
+let estaActivo = true;
+let mensjBienvenida = "bienvenid@";
+let tareaAsignada = null;
+let proximosPasos = undefined;
+const NOMBREUSUSARIO= 'Rodrigo Herrera';
+console.log(mensjBienvenida +' '+NOMBREUSUSARIO + ' hoy tienes '+ cantidadTareas + " tareas con un promedio de "+ calPromedio);
+
+let edadMascota = 8;
+let pesoMascota = 25.4;
+let vacunado = true;
+const NOMBREMASCOTA = 'bola de nieve';
+let duenoActual = null;
+let celularDueno = undefined;
+const ESPECIEMASCOTA = 'canino';
+console.log(NOMBREMASCOTA+ ' es un '+ ESPECIEMASCOTA+ " de "+ edadMascota+" anios"+ ' y pesa '+pesoMascota+" kg.")
+
+let idAlumno = 28987665;
+let promedioAlumno = 86.3;
+let alumnoAprobado = true;
+const NOMBREALUMNO = 'Shamir Hernandez Perez';
+let tieneTutor = true;
+const NOMBREESCUELA = 'Tecnologico de Monterrey';
+console.log("el alumno "+NOMBREALUMNO+ " tiene el promedio "+ promedioAlumno+ " en el "+NOMBREESCUELA);
+
+let productosDisponibles = 12765;
+let precioPromedio = 34.5;
+let hayStock = true;
+let productoEstrella = 'Xiaomi Poco F7 5G';
+let descuentoActual = null;
+let productoEnRevision = undefined;
+const NOMBRETIENDA = "Prime Tech";
+console.log("En "+ NOMBRETIENDA+" el producto mas vendido es "+ productoEstrella+ " a "+ precioPromedio);
+
+let vidas = 5;
+let tiempoJugado = 12;
+let enLinea = true;
+const NOMBREPERSONAJE = "Gosth";
+let ultimoNivelaaaAlcanzado = true;
+let proximoObjetivo = undefined;
+const NOMBREJUEGO = "Call of duty";
+console.log("el personaje "+NOMBREPERSONAJE+" ha estado jugando "+tiempoJugado+" horas en "+NOMBREJUEGO);
+
+let _temperatura = 36;
+let lluviaCaida = 25.7;
+let siLlovio = true;
+let dia = 'martes';
+let registroIncompleto = null;
+let pronostico = undefined;
+const CIUDAD1 = 'Verarcuz';
+console.log('el dia '+dia+" en "+CIUDAD1+" la temperatura fue de "+_temperatura+" C");
+
+let numeroPaginas = 615;
+let calificacion = 9.3;
+let disponibleEnBiblioteca = true;
+const TITULO = "El principito";
+let editor = null;
+let ISBN = undefined;
+const GENERO = "Infantil";
+console.log("El libro "+TITULO+" de genero "+GENERO+" tiene "+numeroPaginas+" paginas");
+
+let likes = 45;
+let tiempoLectura = 34.5;
+let publicado = true;
+let autor = 'usuario1';
+let comentarios = null;
+let hashtag = undefined;
+const NOMBREREDSOCIAL = 'Bestfun';
+console.log('El post de '+autor+" en "+NOMBREREDSOCIAL+" tiene "+likes+" likes");
+
+let numeroDePaquetes = 160;
+let pesoTotal = 60.7;
+let enTransito = true;
+let destino = "Guadalajara";
+let notas = null;
+let codigo = undefined;
+const nombrePaqueteria = "Fedex";
+console.log(nombrePaqueteria+" esta enviando un paquete a "+ destino+" de "+pesoTotal+" kg");
+
+let PersonasEnClase = 34;
+let porcentajeDeAsistencia = 60;
+let laClaseEstaCompleta = false;
+let nombreMateria = "Programacion";
+let profesorTitular = null;
+let aula = undefined;
+const NOMBRECOLEGIO = "tecnologico de minatitlan";
+console.log("En la clase de la materia "+nombreMateria+" en el "+NOMBRECOLEGIO+" asistieron "+PersonasEnClase+" alumnos");
+
+let edadConductor = 45;
+let nivelAlcohol = 23.1;
+let pasoPrueba = true;
+let nombreConductor = 'Pedro';
+let observacionMedica = null;
+let resultadoFinal = undefined;
+const nombreClinica = "Hospital de alta especialidad";
+console.log(nombreConductor+" fue evaluado en "+nombreClinica+" con "+nivelAlcohol+'% de alcohol');
+
+let asientosReservados = 3;
+let precioEstandar = 80;
+let confirmacionEnviada = true;
+let nombreCliente = 'luka modric';
+let comentarioAdicional = null;
+let codigoReserva = undefined;
+const NOMBREAEROLINEA = "Volaris";
+console.log("El cliente "+nombreCliente+" reservo "+asientosReservados+" asientos en "+NOMBREAEROLINEA);
+
+let numeroPagosRealizados = 3;
+let totalPagado = 255.5;
+let pagosCompletos = true;
+let nombreCliente1 = 'jorge';
+let observaciones = null;
+let numeroReferencia = undefined;
+const NOMBREBANCO = 'HSBC';
+console.log(nombreCliente1+" ha pagado "+totalPagado+"$"+" en "+NOMBREBANCO);
+
+let numeroNucleos = 8;
+let velocidadProcesador = 3.2;
+let tieneAntivirus = true;
+let marcaComputadora = 'Asus';
+let softwareAdicional = null;
+let claveDeActivacion = undefined;
+const sistemaOperativo = 'windows 11'
+console.log("El equipo "+ marcaComputadora+" usa "+sistemaOperativo+" con "+numeroNucleos+" nucleos a "+velocidadProcesador+" Ghz");
+
+let numeroIngredientes = 15;
+let aguaEnLitros = 8.2;
+let aptaParaVeganos = false;
+const nombrePlatillo = 'cheescake';
+let ingredienteSecreto = null;
+let comentariosDelChef = undefined;
+const origenPlatillo = 'francia';
+console.log('La receta de '+nombrePlatillo+" de "+origenPlatillo+" lleva "+numeroIngredientes+" ingredientes");
+
+// datos de una red
+let dispositivosConectados = 23;
+let velocidadPromedio = 65;
+let laRedEsEstable = true;
+const nombreRouter = 'Huawei D9909';
+let contrasenaGuardada = null;
+let ipPublica = undefined;
+const proveedorInternet = 'Totalplay';
+console.log('El router de '+proveedorInternet+" tiene " +dispositivosConectados+" dispositivos conectados");
+
+// datos de un examen
+let preguntasRespondidas = 30;
+let puntosObtenidos = 60;
+let aprobado = true;
+let nombreAlumno = 'Sergio Ramos';
+let comentarioDelProfesor = null;
+let numerosIntentos = undefined;
+const NOMBREMATERIA = 'fundamentos de programacion';
+console.log('El alumno '+nombreAlumno+" obtuvo "+puntosObtenidos+" puntos en "+NOMBREMATERIA);
+
+// datos de una suscripcion
+let mesesActivos = 6;
+let costoMensual = 50.5;
+let estaActiva = true;
+let nombreSuscriptor = "Karim benzema";
+let ultimoPago = null;
+let fechaRenovacion = undefined;
+const nombreServicio = 'youtube premium';
+console.log('El usuario '+nombreSuscriptor+" esta suscrito a "+nombreServicio+' por '+mesesActivos+" meses");
+
+// datos de una compra
+let ProductosAdquiridos = 18;
+let precioTotal = 257.60;
+let envioConfirmado = true;
+let nombreComprador = "Iker casillas";
+let cuponApllicado = null;
+let numeroDeGuia = undefined;
+const nombreTienda1 = "mercado liebre";
+console.log('El comprador '+nombreComprador+" compro "+ProductosAdquiridos+" productos por "+precioTotal+'$ en '+nombreTienda1);
+
+// datos de un equipo
+let numeroJugadores = 12;
+let tiempoPromedioPorPartida = 10;
+let juegoActivo = true;
+let nombreEquipo = "mata mancos";
+let entrenador = null;
+let jugadorEstrella = undefined;
+const nombreTorneo = 'tank war';
+console.log('El equipo ' + nombreEquipo + " participa en " + nombreTorneo + " con " + numeroJugadores + " jugadores");
+
+// plataforma de peliculas
+let cantidadPeliculasVistas = 15;
+let promedioDuracion = 180;
+let membresiaActiva = true;
+let nombreUsuario = 'Ronaldo nazario';
+let ultimaPeliculaFavorita = null;
+let resenaPendiente = undefined;
+const nombrePlataforma = 'cinemax4k'
+console.log(nombreUsuario + " ha visto " + cantidadPeliculasVistas + " peliculas" + " en " + nombrePlataforma);
+
+// sistema bancario
+let totalDeCuentas = 65786916;
+let interesMensualPromedio = 12;
+let bancoEnOperacion = true;
+let nombreGerente = 'florentino perez';
+let comentario = null;
+const numeroSucursal = 5437;
+const nombreBanco = 'mexbank'
+console.log("El gerente " + nombreGerente + ' gestiona ' + totalDeCuentas + " cuentas en " + nombreBanco);
+
+// bicicleta
+let numeroDeCambios = 4;
+let pesoBicicleta = 12;
+let tieneFrenoDisco = true;
+let marcaBicicleta = 'benoto';
+let accesorios = null;
+let numeroSerie = undefined;
+const paisFabricacion = 'italia';
+console.log('La bici marca ' + marcaBicicleta + " fabricada en " + paisFabricacion + ' pesa ' + pesoBicicleta + " kg");
+
+// datos de una beca
+let numeroBecarios = 33;
+let montoMensual = 2000;
+let aprobada = true;
+let nombreBeneficiario = 'zinedine zidane';
+let motivoBeca = 'alumnos de escasos recursos';
+let fechaDeposito = undefined;
+const nombrePrograma = 'apoyo para universitarios';
+console.log("El beneficiario " + nombreBeneficiario + " recibe " + montoMensual + "$ pesos mensuales del programa " + nombrePrograma);
+
+// sistema de reserva de hotel
+let nochesReservadas = 3;
+let costoPorNoche = 1250;
+let reservacionConfirmada = true;
+let nombreHuesped = 'mesut otzil';
+let comentarios2 = null;
+let numeroHabitacion = undefined;
+const nombreHotel = 'gold royal resort';
+console.log("El huesped " + nombreHuesped + " reservo " + nochesReservadas + ' noches en el ' + nombreHotel);
+
+// datos de una app movil
+let numeroDeDescargas = 100000;
+let valoracionPromedio = 4.5;
+let disponibleEnAndroid = true;
+let nombreAplicacion = 'skateguard';
+let funcionPrincipal = null;
+let version = undefined;
+const empresaDesarrolladora = 'OpenTIcompany';
+console.log('La app ' + nombreAplicacion + " de " + empresaDesarrolladora + " tiene " + numeroDeDescargas + " descargas");
+
+// datos de una sesion medica
+let diasTratamiento = 15;
+let dosisDiaria = 2.5;
+let tratamientoEnCurso = true;
+let nombrePaciente = 'Daniel Carvajal';
+let alergias = null;
+let numeroExpediente = undefined;
+const nombreHospital = 'clinic Star royal';
+console.log(nombrePaciente + " esta en tratamiento en el " + nombreHospital + " por " + diasTratamiento + " dias");
+
+//datos de una tienda online
+let productosCarrito = 3;
+let totalEstimado = 8500.5;
+let compraCompletada = true;
+let nombreComprador1 = 'Tony kroos';
+let comentariosVendedor = null;
+let enlacePago = undefined;
+const nombreWeb = 'mercado mexpay';
+console.log(nombreComprador1 + " tiene " + productosCarrito + ' productos por $' + totalEstimado + " en " + nombreWeb);
+
+// datos de un curso en linea
+let numeroModulos = 50;
+let horasTotales = 365;
+let cursoFinalizado = true;
+let nombreEstudiante = 'Gareth bale';
+let retroalimentacion = null;
+let codigoCurso = undefined;
+const nombrePlataforma1 = 'udemy';
+console.log(nombreEstudiante + " completo " + numeroModulos + " modulos en " + nombrePlataforma1);
+
+// compra de una tienda local
+let numArtVnedidos = 7;
+let ingresoTotal = 63.5;
+let ventaCompletada = true;
+let nombreVendedor = 'robert lewandoski';
+let comentariosCliente = null;
+let codigoFactura = undefined;
+const nombreLocal = 'abarrotes tonita';
+console.log(nombreVendedor + ' vendio '+ numArtVnedidos + ' articulos en ' + nombreLocal);
+
+// datos de un post de un blog
+let comentariosRecibidos = 140;
+let tiempoPromLectura = 10;
+let publicado1 = true;
+let tituloPost = 'problemas de ciberseguridad';
+let autorInvitado = null;
+let palabrasClave = undefined;
+const nombreBlog = 'la progrmacion profesional';
+console.log('El post ' + tituloPost + ' en ' + nombreBlog + " tiene " + comentariosRecibidos + " comentarios");
+
+//datos de un paciente
+let numeroConsultasMedicas = 8;
+let alturaEnMetros = 1.70;
+let seguroActivo = true;
+let nombrePaciente1 = 'Diego maradona';
+let alergiasConocidas = null;
+let numeroPoliza = undefined;
+const nombreHospital1 = 'star medica';
+console.log(nombrePaciente1 + " ha tenido " + numeroConsultasMedicas + ' consultas en ' + nombreHospital1);
+
+// datos sobre una pelicula
+let duracionEnMinutos = 180;
+let calificacionUsuarios = 4.2;
+let paraTodoPulbico = true;
+let tituloPelicula = 'transformers';
+let actorPrincipal = null;
+let idPelicula = undefined;
+const nombrePlataforma2 = 'pelimax';
+console.log('La pelicula ' + tituloPelicula + " esta disponible en " + nombrePlataforma2);
+
+// datos de una cuenta bancaria 
+let transferenciasRealizadas = 30;
+let saldoDisponible = 188122;
+let cuentaActiva = true;
+let nombreTitular = 'xabi alonso';
+let ultimatransefercia = null;
+let numeroCuenta = undefined;
+const nombreBanco1 = 'santander';
+console.log(nombreTitular + " tiene $" + saldoDisponible + " disponible en " + nombreBanco1);
+
+// datos de un servidor 
+let usuariosConectados = 343;
+let usoPromCpu = 60.4;
+let servidorOnline = true;
+let nombreServidor = 'company jacob & co';
+let ultimaActualizacion = null;
+let ipServidor = undefined;
+const nombreProveedor = 'totalplay';
+console.log('El servidor ' + nombreServidor + ' esta alojado en ' + nombreProveedor);
+
+// datos de una tienda de ropa 
+let prendasDisponibles = 234;
+let precioPromPrenda = 120;
+let tiendaAbierta = true;
+let nombreGerente1 = 'carlo ancelotti';
+let promocionActual = null;
+let idSucursal = undefined;
+const nombretienda2 = 'ropa chida1'
+console.log(nombreGerente1 + " administra " + prendasDisponibles + " prendas en " + nombretienda2);
+
+// informacion sobre un auto
+let numeroPuertas = 4;
+let capacidadTanque = 30;
+let estaEncendido = true;
+let marcaAuto = 'BMW';
+let ultimoDueno = null;
+let numeroSerie1 = undefined;
+const paisFabricacion89 = 'Alemania'
+console.log('El auto marca ' + marcaAuto + ' fue fabricado en ' + paisFabricacion89 + ' y tiene ' + numeroPuertas + ' puertas');
+
+// detalles de operacion de una biblioteca
+let librosPrestadosHoy = 145;
+let horasOperacion = 8;
+let bibliotecaAbierta = true;
+let nombreBibliotecario = 'Serge Nabry';
+let libroDestacado = null;
+let idCliente = undefined;
+const nombreBilbioteca = 'libro center';
+console.log(nombreBibliotecario + ' trabaja en ' + nombreBilbioteca + ', donde se prestaron ' + librosPrestadosHoy + ' libros hoy.');
+
+// simula datos de una pagina web
+let visitasDiarias = 345;
+let tiempoPromedioPorSesion = 15;
+let sitioActivo = true;
+const nombreDelSitio = 'visual write';
+let ultimaActualizacion1 = null;
+const URL = undefined;
+const empresaDesarrolladora1 = 'Modern Start Corp';
+console.log('El sitio ' + nombreDelSitio + ' recibio ' + visitasDiarias + ' visitas hoy, desarrollado por ' + empresaDesarrolladora1);
+
+// aplicacion de mensajeria 
+let cantidadMensajesEnviados = 546;
+let tiempoPromedioPorMensaje = 2;
+let estaConectado = true;
+let nombreUsuario1 = 'Alfonso Davies';
+let mensajeDestacado = null;
+let estadoConexion = undefined;
+const nombreAPP = 'QEPASS';
+console.log(nombreUsuario1 + ' envio ' + cantidadMensajesEnviados + ' mensajes en ' + nombreAPP);
+
+// control de inventario
+let unidadesEnStock = 2345;
+let precioPorUnidad = 800;
+let disponibleParaVenta = true;
+let nombreProducto = 'realme gt 7';
+let comentariosProducto = null;
+let codigoProducto = undefined;
+const almacen = 'shop 1 tlanepantla';
+console.log('El producto ' + nombreProducto + ' cuesta ' + precioPorUnidad + ' y esta en el almacen ' + almacen);
+
+// reserva de transporte
+let numeroPasajeros = 40;
+let precioBoleto = 1240;
+let viajeCconfirmado = true; 
+let nombrePasajero = 'picolin palacios';
+let observaciones1 = null;
+let asientoAsignado = undefined;
+const nombrelinea = 'gulf royal bus';
+console.log(nombrePasajero + ' reservo un boleto en ' + nombrelinea + ' por ' + precioBoleto + ' pesos');
+
+// curso en linea
+let numeroModulosCompletados = 30;
+let horasEstudio = 80;
+let cursoTerminado = true;
+let nombreAlumno1 = 'ngolo kante';
+let comentarioProfesor = null;
+let codigocurso1 = undefined;
+const nombrePlataforma3 = 'curso total';
+console.log(nombreAlumno1 + ' completo ' + numeroModulosCompletados + ' modulos en ' + nombrePlataforma3 );
+
+// sistema de salud
+let numeroMedicamentosRecetados = 5;
+let dosisDiariaMg = 15;
+let tratamientoEnCurso1 = true;
+let nombrePaciente2 = 'miroslav klose';
+let reaccionesAdversas = null;
+let historiaClinica = undefined;
+const nombreHospital2 = 'clinic center';
+console.log(nombrePaciente2 + ' tiene un tratamiento activo en ' + nombreHospital2);
+
+// desarrollo de videojuego
+let nivelesSuperados = 50;
+let tiempoTotalJugado = 50;
+let sesionActiva = true;
+let nombreJugador = 'memo ochoa';
+let ultimoLogroDesbloqueado = null;
+let idUsuario = undefined;
+const nombreJuego = 'asphalt race future';
+console.log(nombreJugador + " jugo " + tiempoTotalJugado + ' horas en ' + nombreJuego);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
